7116e013af0dc8384d1521fd8a8a65807f2046fb
vew27 <vew271@gmail.com>
2021-06-08 23:22:02 +0700

    second commit

--- app/src/main/java/com/example/packeco/detail/detailvendor/DetailVendor.kt
+++ app/src/main/java/com/example/packeco/detail/detailvendor/DetailVendor.kt
@@ -2,15 +2,56 @@ package com.example.packeco.detail.detailvendor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.packeco.databinding.ActivityDetailPackagingBinding
+import com.example.packeco.R
 import com.example.packeco.databinding.ActivityDetailVendorBinding
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
 
 class DetailVendor : AppCompatActivity() {
+    private lateinit var mapboxMap: MapboxMap
     private lateinit var binding: ActivityDetailVendorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.mapbox_token))
         binding = ActivityDetailVendorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.map.onCreate(savedInstanceState)
+        binding.map.getMapAsync { mapboxMap ->
+            this.mapboxMap = mapboxMap
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.map.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+        binding.map.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        binding.map.onPause()
+    }
+    override fun onStop() {
+        super.onStop()
+        binding.map.onStop()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.map.onSaveInstanceState(outState)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.map.onDestroy()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.map.onLowMemory()
     }
 }
--- app/src/main/java/com/example/packeco/home/MainActivity.kt
+++ app/src/main/java/com/example/packeco/home/MainActivity.kt
@@ -1,27 +1,90 @@
 package com.example.packeco.home
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
+import com.example.packeco.R
 import com.example.packeco.databinding.ActivityMainBinding
 import com.example.packeco.detail.detailpackaging.DetailPackaging
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
+    private var clicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btPhoto.setOnClickListener{
+        binding.fabPhoto.setOnClickListener{
+            onAddButtonClicked()
+        }
+
+        binding.fabGalery.setOnClickListener{
+            Log.d("mssg", "button pressed")
+            var intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 100)
+        }
+
+        binding.fabCamera.setOnClickListener{
             Toast.makeText(applicationContext, "Take Photo", Toast.LENGTH_SHORT).show()
         }
 
         binding.cvItem.setOnClickListener{
             val intent = Intent(this@MainActivity, DetailPackaging::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun onAddButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked){
+            binding.fabGalery.startAnimation(fromBottom)
+            binding.fabCamera.startAnimation(fromBottom)
+            binding.fabPhoto.startAnimation(rotateOpen)
+        }else{
+            binding.fabGalery.startAnimation(toBottom)
+            binding.fabCamera.startAnimation(toBottom)
+            binding.fabPhoto.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if (!clicked){
+            binding.fabGalery.visibility = View.VISIBLE
+            binding.fabCamera.visibility = View.VISIBLE
+        }else{
+            binding.fabGalery.visibility = View.INVISIBLE
+            binding.fabCamera.visibility = View.INVISIBLE
+        }
+    }
+
+    /** Check if this device has a camera */
+    private fun checkCameraHardware(context: Context): Boolean {
+        if (context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true
+        } else {
+            // no camera on this device
+            return false
         }
     }
 }
